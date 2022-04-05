4785140458f28cfda850c63810e39f286cd9e9c3
Silvan Mosberger <contact@infinisil.com>
2022-04-06 00:27:00 +0200

    Reproducibility

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
 { name, fortune }:
-with import <nixpkgs> {};
+with import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/e4501868fb33a78bcb2dd15c9893720a460d22ed") {};
 
 writeText name fortune
